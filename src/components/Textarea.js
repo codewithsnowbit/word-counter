1f3a09130387a3b1bb7f2a6190f964260f8a2277
SnowBit <65452005+snowbit-coderboi@users.noreply.github.com>
2021-10-05 09:08:37 +0530

    Added auto-resize textarea

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import TextareaAutosize from "react-textarea-autosize";
 
 class Textarea extends Component {
   constructor() {
@@ -9,26 +10,14 @@ class Textarea extends Component {
     this.countWords = this.countWords.bind(this);
   }
   countWords() {
-    // Get the input text value
     var text = document.getElementById("data").value;
-
-    // Initialize the word counter
     var numWords = 0;
-
-    // Loop through the text
-    // and count spaces in it
     for (var i = 0; i < text.length; i++) {
       var currentCharacter = text[i];
-
-      // Check if the character is a space
       if (currentCharacter === " ") {
         numWords += 1;
       }
     }
-
-    // Add 1 to make the count equal to
-    // the number of words
-    // (count of words = count of spaces + 1)
     numWords += 1;
 
     // Display it as output
@@ -41,12 +30,9 @@ class Textarea extends Component {
   render() {
     return (
       <div>
-        <textarea
-          id="data"
-          cols="30"
-          rows="10"
-          onInput={this.countWords}
-        ></textarea>
+        {/* <textarea cols="30" rows="10" onInput={this.countWords}></textarea> */}
+        <TextareaAutosize id="data" onInput={this.countWords} />
+
         <h2>word: {this.state.wordCount}</h2>
       </div>
     );
