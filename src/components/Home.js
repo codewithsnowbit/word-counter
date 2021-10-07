d412f1766eafad191ad411ffc55ca912d9d0d0e8
SnowBit <65452005+snowbit-coderboi@users.noreply.github.com>
2021-10-07 10:17:55 +0530

    Added title animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import "../styles/Home.css";
+import "animate.css";
 
 function Home() {
   return (
     <div>
       <div className="globe"></div>
       <br />
-      <h2 className="text-center">
+      <h2 className="text-center animate__animated animate__tada">
         Welcome to the Word Counter🎉,{" "}
         <Link to="/app">
           <span className="text-decoration-underline span">Let's go </span>{" "}
