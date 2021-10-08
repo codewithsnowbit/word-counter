8c5980e60e7aa76143aa8ef81430c27f33cac158
SnowBit <65452005+snowbit-coderboi@users.noreply.github.com>
2021-10-08 12:45:12 +0530

    Added confetti

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import Confetti from "react-confetti";
 import "../styles/Home.css";
+import useWindowSize from "react-use/lib/useWindowSize";
 import "animate.css";
 
 function Home() {
+  const { width, height } = useWindowSize();
   return (
     <div>
       <div className="globe"></div>
       <br />
+      <Confetti width={width} height={height} recycle={false} />
       <h2 className="text-center animate__animated animate__tada">
         Welcome to the Word Counter🎉,{" "}
         <Link to="/app">
