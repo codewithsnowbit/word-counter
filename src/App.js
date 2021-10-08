62563a662f36b204cf02926f975d74946d35b32f
SnowBit <65452005+snowbit-coderboi@users.noreply.github.com>
2021-10-08 13:50:30 +0530

    Added 404 page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Textarea from "./components/Textarea";
 import Home from "./components/Home";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import fournotfour from "./components/404";
 export default function App() {
   return (
     <div className="container">
       <Router>
-        <Route path="/" exact component={Home} />
-        <Route path="/app" exact component={Textarea} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/app" exact component={Textarea} />
+          <Route component={fournotfour} />
+        </Switch>
       </Router>
     </div>
   );
